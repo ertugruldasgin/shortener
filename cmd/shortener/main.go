63abb326cc17ec94fa86756db0517191c81dcff9
Umut Ertuğrul Daşgın <105071264+ertugruldasgin@users.noreply.github.com>
2026-08-19 23:48:52 +0300

    wire service and handlers in main

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,9 +1,24 @@
 package main
 
-import "fmt"
-
-const name = "Shortener"
+import (
+	"ertugruldasgin/shortener/internal/httpapi"
+	"ertugruldasgin/shortener/internal/link"
+	"ertugruldasgin/shortener/internal/memstore"
+	"ertugruldasgin/shortener/internal/slug"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Printf("Hello, %s!", name)
+	repo := memstore.New()
+	gen := slug.New()
+	svc := link.NewService(repo, gen)
+	h := httpapi.New(svc)
+
+	addr := ":8080"
+	log.Printf("listening on %s", addr)
+
+	if err := http.ListenAndServe(addr, h.Routes()); err != nil {
+		log.Fatal(err)
+	}
 }
