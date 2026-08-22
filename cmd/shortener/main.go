e9bf8249c74cf6ddd37c637569036680508ddb24
Umut Ertuğrul Daşgın <105071264+ertugruldasgin@users.noreply.github.com>
2026-08-22 20:45:32 +0300

    feat: add graceful shutdown

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"ertugruldasgin/shortener/internal/httpapi"
 	"ertugruldasgin/shortener/internal/link"
 	"ertugruldasgin/shortener/internal/postgres"
@@ -9,18 +10,27 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 var version = "dev"
 
+const shutdownTimeout = 10 * time.Second
+
 func main() {
 	log.Printf("shortener %s starting", version)
+
 	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
 		log.Fatal("DATABASE_URL is not set")
 	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
 
 	pool, err := pgxpool.New(context.Background(), dsn)
 	if err != nil {
@@ -36,14 +46,30 @@ func main() {
 	gen := slug.New()
 	svc := link.NewService(repo, gen)
 	recorder := link.NewClickRecorder(repo)
-	defer recorder.Close()
-
 	h := httpapi.New(svc, recorder, version)
 
-	addr := ":8080"
-	log.Printf("listening on %s", addr)
-
-	if err := http.ListenAndServe(addr, h.Routes()); err != nil {
-		log.Fatal(err)
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: h.Routes(),
 	}
+
+	go func() {
+		log.Printf("listening on %s", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("serving: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+	log.Print("shutting down")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer cancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Printf("shutdown: %v", err)
+	}
+
+	recorder.Close()
+	log.Print("stopped")
 }
